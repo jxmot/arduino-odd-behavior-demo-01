89328c46775802fb36f8a3359fcd54783a1f1329
jxmot <webdev@stupiddog.net>
2017-09-24 02:52:52 -0500

    initial commit of demo ino files

---
+++ typedefstruct_fail/typedefstruct_fail.ino
@@ -0,0 +1,24 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+typedef struct {
+   const char* name;
+   int time;
+   float value;
+} SensorData;
+
+SensorData sdata;
+
+bool testfunc(SensorData &_data) {
+
+}
+
+
---
+++ typedefstruct_pass/typedefstruct_pass.ino
@@ -0,0 +1,24 @@
+
+typedef struct {
+   const char* name;
+   int time;
+   float value;
+} SensorData;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+SensorData sdata;
+
+bool testfunc(SensorData &_data) {
+
+}
+
+
